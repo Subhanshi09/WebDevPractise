ab13dd818b4ac3f13480e88156a69a8004253910
Subhanshi09 <81361592+Subhanshi09@users.noreply.github.com>
2021-08-28 15:52:51 +0530

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,31 @@
+const buttons=document.querySelectorAll('button');
+const screen=document.getElementById('screen');
+
+
+
+for(let btn of buttons){
+    btn.addEventListener('click',(e)=>{
+        const buttonText=e.target.innerText;
+        
+        if(buttonText==='x')
+        {
+            screen.value+='*';
+        }
+        else if(buttonText==='C'){
+            screen.value="";
+        }
+        else if(buttonText=== '=')
+        {
+            try{
+                screen.value=eval(screen.value);
+            }catch(e){
+                screen.value="Invalid Operation";
+            }
+            screen.value=eval(screen.value);
+        }
+        else{
+            screen.value+=buttonText;
+        }
+    })
+}
+
